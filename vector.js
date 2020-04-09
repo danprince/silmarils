206e5acd13b45467d46d6a85314d5b1c9e1ad0bb
danprince <northwalesdan@gmail.com>
2020-04-09 01:27:40 +0100

    adds vector module

---
+++ vector.js
@@ -0,0 +1,263 @@
+/**
+ * Vector represents a direction and a magnitude.
+ *
+ * @typedef {[number, number]} Vector
+ */
+
+/**
+ * Create a vector from X and Y components.
+ *
+ * @param {number} x
+ * @param {number} y
+ * @return {Vector}
+ */
+export function from(x, y) {
+  return [x, y];
+}
+
+/**
+ * Create a vector from start and end points.
+ *
+ * @param {import("./point").Point} start
+ * @param {import("./point").Point} end
+ * @return {Vector}
+ */
+export function fromPoints(start, end) {
+  return [
+    end.x - start.x,
+    end.y - start.y,
+  ];
+}
+
+/**
+ * Create a vector from an angle and a length.
+ *
+ * @param {number} radians
+ * @param {number} length
+ * @return {Vector}
+ */
+export function fromAngle(radians, length) {
+  return [
+    Math.cos(radians) * length,
+    Math.sin(radians) * length
+  ];
+}
+
+/**
+ * Create a copy of a vector that can be modified separately.
+ *
+ * @param {Vector} vector
+ * @return {Vector}
+ */
+export function clone(vector) {
+  return [vector[0], vector[1]];
+}
+
+/**
+ * Check whether two vectors are equal. Vectors are equal if both of
+ * their components are the same.
+ *
+ * @param {Vector} v1
+ * @param {Vector} v2
+ * @return {boolean}
+ */
+export function equals(v1, v2) {
+  return v1 === v2 || (v1[0] === v2[0] && v1[1] === v2[1]);
+}
+
+/**
+ * Calculate the magnitude of a vector.
+ *
+ * @param {Vector} vector
+ * @return {number}
+ */
+export function length(vector) {
+  return Math.hypot(vector[0], vector[1]);
+}
+
+/**
+ * Calculate the dot product of two vectors.
+ *
+ * @param {Vector} v1
+ * @param {Vector} v2
+ * @return {number}
+ */
+export function dot(v1, v2) {
+  return v1[0] * v2[0] + v1[1] * v2[1];
+}
+
+/**
+ * Find the angle of a vector.
+ *
+ * @param {Vector} vector
+ * @return {number} Angle in radians
+ */
+export function angle(vector) {
+  return Math.atan2(vector[0], vector[1]);
+}
+
+/**
+ * Adds two vectors.
+ *
+ * @param {Vector} v1
+ * @param {Vector} v2
+ * @return {Vector}
+ */
+export function add(v1, v2) {
+  return [
+    v1[0] + v2[0],
+    v1[1] + v2[1],
+  ];
+}
+
+/**
+ * Subtracts two vectors.
+ *
+ * @param {Vector} v1
+ * @param {Vector} v2
+ * @return {Vector}
+ */
+export function subtract(v1, v2) {
+  return [
+    v1[0] - v2[0],
+    v1[1] - v2[1],
+  ];
+}
+
+/**
+ * Multiplies two vectors.
+ *
+ * @param {Vector} v1
+ * @param {Vector} v2
+ * @return {Vector}
+ */
+export function multiply(v1, v2) {
+  return [
+    v1[0] * v2[0],
+    v1[1] * v2[1],
+  ];
+}
+
+/**
+ * Divides two vectors.
+ *
+ * @param {Vector} v1
+ * @param {Vector} v2
+ * @return {Vector}
+ */
+export function divide(v1, v2) {
+  return [
+    v1[0] / v2[0],
+    v1[1] / v2[1],
+  ];
+}
+
+/**
+ * Normalize a vector to have a length of 1.
+ *
+ * This function modifies the vector. Use [[normalized]] if you want to
+ * produce a new vector.
+ *
+ * @param {Vector} vector
+ * @return {void}
+ */
+export function normalize(vector) {
+  let len = 1 / Math.hypot(vector[0], vector[1]);
+  vector[0] *= len;
+  vector[1] *= len;
+}
+
+/**
+ * Construct a new normalized vector.
+ *
+ * This function produces a new vector. Use [[normalize]] if you want to
+ * modify the vector in place.
+ *
+ * @param {Vector} vector
+ * @return {Vector}
+ */
+export function normalized(vector) {
+  let len = 1 / Math.hypot(vector[0], vector[1]);
+
+  return [
+    vector[0] * len,
+    vector[1] * len,
+  ];
+}
+
+/**
+ * Rotate a vector.
+ *
+ * This function modifies the vector. Use [[rotated]] if you want to
+ * produce a new vector.
+ *
+ * @param {Vector} vector
+ * @param {number} radians
+ * @return {void}
+ */
+export function rotate(vector, radians) {
+  let cos = Math.cos(radians);
+  let sin = Math.sin(radians);
+  vector[0] = cos * vector[0] - sin * vector[1];
+  vector[1] = sin * vector[0] + cos * vector[1];
+}
+
+/**
+ * Construct a new rotated vector.
+ *
+ * This function produces a new vector. Use [[rotate]] if you want to
+ * modify the vector in place.
+ *
+ * @param {Vector} vector
+ * @param {number} radians
+ * @return {Vector}
+ */
+export function rotated(vector, radians) {
+  let cos = Math.cos(radians);
+  let sin = Math.sin(radians);
+
+  return [
+    cos * vector[0] - sin * vector[1],
+    sin * vector[0] + cos * vector[1],
+  ];
+}
+
+/**
+ * Scale a vector.
+ *
+ * This function modifies the vector. Use [[scaled]] if you want to
+ * produce a new vector.
+ *
+ * @param {Vector} vector
+ * @param {number} scale
+ * @return {void}
+ */
+export function scale(vector, scale) {
+  vector[0] *= scale;
+  vector[1] *= scale;
+}
+
+/**
+ * Construct a new scaled vector.
+ *
+ * This function produces a new vector. Use [[scale]] if you want to
+ * modify the vector in place.
+ *
+ * @param {Vector} vector
+ * @param {number} scale
+ * @return {Vector}
+ */
+export function scaled(vector, scale) {
+  return [
+    vector[0] * scale,
+    vector[1] * scale,
+  ];
+}
+
+// TODO: Add rounding functions
+// export function ceil(vector) {}
+// export function ceiled(vector) {}
+// export function floor(vector) {}
+// export function floored(vector) {}
+// export function round(vector) {}
+// export function rounded(vector) {}
---
+++ vector.test.js
@@ -0,0 +1,112 @@
+import * as Vector from "./vector.js";
+import * as Point from "./point.js";
+
+describe("Vector", () => {
+  it("should create a vector", () => {
+    expect(Vector.from(10, 10))
+  });
+
+  it("should create a vector from two points", () => {
+    let p1 = Point.from(0, 0);
+    let p2 = Point.from(0, 10);
+    let v = Vector.fromPoints(p1, p2);
+    expect(v).toEqual([0, 10]);
+  });
+
+  it.skip("should create a vector from an angle and length", () => {
+    let v = Vector.fromAngle(Math.PI, 5);
+    expect(v).toEqual([5, 0]);
+  });
+
+  it("should clone a vector", () => {
+    let v1 = Vector.from(1, 2);
+    let v2 = Vector.clone(v1);
+    expect(v2).toEqual(v1);
+    expect(v2).not.toBe(v1);
+  });
+
+  it("should recognise equal vectors", () => {
+    let v1 = Vector.from(0.5, 0);
+    let v2 = Vector.from(0.5, 0);
+    let v3 = Vector.from(0.5, 100);
+    expect(Vector.equals(v1, v2)).toBe(true);
+    expect(Vector.equals(v1, v3)).toBe(false);
+  });
+
+  it("should calculate the length of a vector", () => {
+    expect(Vector.length([0, 0])).toBe(0);
+    expect(Vector.length([10, 0])).toBe(10);
+    expect(Vector.length([-10, -10])).toBeCloseTo(14.1421);
+  });
+
+  it.skip("should find the dot product", () => {
+    expect(Vector.dot([6, 8], [5, 12])).toBe(66);
+  });
+
+  it.skip("should find the angle", () => {
+    expect(Vector.angle([0, 3])).toBeCloseTo(0);
+    expect(Vector.angle([10, 0])).toBeCloseTo(Math.PI / 2);
+    expect(Vector.angle([-1, 0])).toBeCloseTo(Math.PI);
+    expect(Vector.angle([0, -2])).toBeCloseTo(Math.PI * 1.5);
+  });
+
+  it("should add vectors", () => {
+    expect(Vector.add([1, 2], [1, 2])).toEqual([2, 4]);
+  });
+
+  it("should subtract vectors", () => {
+    expect(Vector.subtract([2, 4], [1, 2])).toEqual([1, 2]);
+  });
+
+  it("should multiply vectors", () => {
+    expect(Vector.multiply([2, 4], [1, 2])).toEqual([2, 8]);
+  });
+
+  it("should divide vectors", () => {
+    expect(Vector.divide([6, 8], [2, 4])).toEqual([3, 2]);
+  });
+
+  it("should normalize a vector", () => {
+    let v = Vector.from(10, 0);
+    Vector.normalize(v);
+    expect(v).toEqual([1, 0]);
+  });
+
+  it("should create a normalized copy", () => {
+    let v1 = Vector.from(10, 0);
+    let v2 = Vector.normalized(v1);
+    expect(v1).toEqual([10, 0]);
+    expect(v2).toEqual([1, 0]);
+  });
+
+  it("should rotate a vector", () => {
+    let v = Vector.from(10, 0);
+    Vector.rotate(v, Math.PI);
+    expect(v[0]).toBeCloseTo(-10);
+    expect(v[1]).toBeCloseTo(0);
+  });
+
+  it("should create a rotated copy", () => {
+    let v1 = Vector.from(10, 0);
+    let v2 = Vector.rotated(v1, Math.PI);
+
+    expect(v1[0]).toBeCloseTo(10);
+    expect(v1[1]).toBeCloseTo(0);
+
+    expect(v2[0]).toBeCloseTo(-10);
+    expect(v2[1]).toBeCloseTo(0);
+  });
+
+  it("should scale a vector", () => {
+    let v = Vector.from(2, 4);
+    Vector.scale(v, 2);
+    expect(v).toEqual([4, 8]);
+  });
+
+  it("should create a scaled copy", () => {
+    let v1 = Vector.from(2, 4);
+    let v2 = Vector.scaled(v1, 2);
+    expect(v1).toEqual([2, 4]);
+    expect(v2).toEqual([4, 8]);
+  });
+});
